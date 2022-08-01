14e3bdaee28e6a25ac747273402187235ba26421
ArpitB95 <arpit.bhatt82@gmail.com>
2022-08-01 16:56:45 +0100

    Task file pushed from localhost

---
+++ Task.py
@@ -0,0 +1,29 @@
+# User data - Takes data and will give output
+
+
+print('Hi user')
+print('Please enter your First name')
+First_Name = input()
+
+print('Please enter your Last name')
+Last_Name =input()
+
+print('Please enter your Age')
+Age = int(input())
+
+print('Please enter your Address')
+Adress = str(input())
+
+
+print('Please enter your Salary')
+Salary = int(input())
+
+print('Please enter your course')
+Course = input()
+
+print('Please enter your enrollment number')
+Enroll_No= int(input())
+
+
+
+print( First_Name + " " + Last_Name + " " + str(Age) + " " + Adress + " " + str(Salary) + " " + Course + " " + str(Enroll_No))
